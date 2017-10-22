03fc0433e46662a620056775360feccec99c3b21
Vicargo <vicargoapp@gmail.com>
2017-10-22 16:41:39 +0200

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 Platzimusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+</template>
+
+<script>
+import Artist from './components/Artist.vue' //al ser index.js no hace falta poner la ruta entera. 
+import Spinner from './components/Spinner.vue' //al ser index.js no hace falta poner la ruta entera. 
+import getArtists from './api' //al ser index.js no hace falta poner la ruta entera. 
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        {name: 'Argentina', value: 'argentina'},
+        {name: 'Colombia', value: 'colombia'},
+        {name: 'España', value: 'spain'}
+      ],
+      selectedCountry: 'argentina',
+      loading: true
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtist(){
+      const self = this; //En esta scope this hace referencia al componente. 
+      this.loading = true
+      this.artists = []
+      getArtists(this.selectedCountry)
+        .then(function (artists){
+          //this scope global,
+          self.loading = false
+          self.artists = artists
+          
+        })
+    }
+  },
+  mounted() {
+    this.refreshArtist();
+  },
+  watch: {
+    selectedCountry(){
+      this.refreshArtist();
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+	#app
+		font-family 'Avenir', Helvetica, Arial, sans-serif
+		-webkit-font-smoothing antialiased
+		-moz-osx-font-smoothing grayscale
+		text-align center
+		color #2c3e50
+		margin-top 60px
+
+	h1, h2
+		font-weight normal
+
+	ul
+		list-style-type none
+		padding 0
+
+	li
+		display inline-block
+		margin 0 10px
+
+	a
+		color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	Platzimusic
+// API key	9760c5c07a11bc55a5007c411331c2c9
+// Shared secret	7d7a8f8c17e1a503536c4df01d92950d
+// Registered to	Vicargo
+
+export default {
+    API_NAME: 'Platzimusic',
+    API_KEY: '9760c5c07a11bc55a5007c411331c2c9',
+    API_SECRET: '7d7a8f8c17e1a503536c4df01d92950d',
+    API_REGISTREDTO: 'Vicargo'
+}
